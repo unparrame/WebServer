9f8c7a5e4c1b48b72db4768c2fd3ae47f35c86b7
vinsensiusvey <vinsensiusvey@gmail.com>
2015-10-23 20:25:56 +0700

    Perbaikan postsmodel

--- www/application/models/postsmodel.php
+++ www/application/models/postsmodel.php
@@ -4,7 +4,7 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 
-class Postersmodel extends CI_Model {
+class Postsmodel extends CI_Model {
 	public $posterEmail;
 	public $state;
 	public $image;
