77f1e4d0d9dfa31ec3f413bdf168160384773cb4
Pascal Alfadian Nugroho <pascalalfadian@live.com>
2015-10-24 17:10:31 +0700

    Citizen charts based on database

--- www/application/controllers/Citizens.php
+++ www/application/controllers/Citizens.php
@@ -5,7 +5,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Citizens extends CI_Controller {
 
     public function index() {
-        $this->load->view('citizens_list');
+        $this->load->model('Citizensmodel');
+        $this->db->select('nik, nama');
+        $query = $this->db->get('citizens');
+        $citizens = array();
+        foreach($query->result() as $row) {
+            $citizens[] = $row;
+        }
+        $data = array(
+            'citizens' => $citizens,
+            'chartjsAgama' => $this->Citizensmodel->chartJsAgama(),
+            'chartjsJenisKelamin' => $this->Citizensmodel->chartJsJenisKelamin()
+        );
+        $this->load->view('citizens_list', $data);
     }
 
 }
--- www/application/views/citizens_list.php
+++ www/application/views/citizens_list.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Citizens Analytics</title>
+        <title>Data Penduduk</title>
         <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
         <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -28,14 +28,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <body>
         <div class="container">
             <div class="row">
-                <div class="col-md-3">
-                    <h3>Usia &amp; Jenis Kelamin</h3>
-                    <canvas id="ageChart" width="300" height="200"></canvas>
-                    <span id="ageChartLegend"></span>
-                    <h3>Agama</h3>
-                    <canvas id="religionChart" width="300" height="200"></canvas>
-                    <span id="religionChartLegend"></span>
-                </div>
                 <div class="col-md-9">
                     <table class="table">
                         <thead>
@@ -46,35 +38,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>123456789</td>
-                                <td>Pascal Alfadian Nugroho</td>
-                                <td>
-                                    <button type="button" class="btn btn-default" aria-label="Lihat Detail">
-                                        <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>234567890</td>
-                                <td>Vinsensius Kevin</td>
-                                <td>
-                                    <button type="button" class="btn btn-default" aria-label="Lihat Detail">
-                                        <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>345678901</td>
-                                <td>Wilianto Indrawan</td>
-                                <td>
-                                    <button type="button" class="btn btn-default" aria-label="Lihat Detail">
-                                        <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-                                    </button>
-                                </td>
-                            </tr>
+                            <?php foreach ($citizens as $citizen): ?>
+                                <tr>
+                                    <td><?= $citizen->nik ?></td>
+                                    <td><?= $citizen->nama ?></td>
+                                    <td>
+                                        <a href="/citizens/<?= $citizen->nik ?>">
+                                            <button type="button" class="btn btn-default" aria-label="Lihat Detail">
+                                                <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+                                            </button>
+                                        </a>
+                                    </td>
+                                </tr>                            
+                            <?php endforeach; ?>
                         </tbody>
                     </table>
+                </div>
+                <div class="col-md-3">
+                    <h3>Jenis Kelamin</h3>
+                    <canvas id="genderChart" width="300" height="200"></canvas>
+                    <span id="genderChartLegend"></span>
+                    <h3>Agama</h3>
+                    <canvas id="religionChart" width="300" height="200"></canvas>
+                    <span id="religionChartLegend"></span>
                 </div>
             </div>
         </div>
@@ -83,64 +69,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <script src="/assets/js/Chart.js"></script>
         <script>
             $(document).ready(function () {
-                var ageData = {
-                    labels: ["0-20", "21-40", "41-60", "61-80", "81-100", "101+"],
-                    datasets: [
-                        {
-                            label: "Pria",
-                            fillColor: "pink",
-                            data: [5, 7, 15, 9, 8, 1]
-                        },
-                        {
-                            label: "Wanita",
-                            fillColor: "blue",
-                            data: [1, 13, 14, 8, 6, 2]
-                        }
+                var genderData = <?= json_encode($chartjsJenisKelamin); ?>;
+                var genderChartElement = $("#genderChart");
+                var genderChartContext = genderChartElement.get(0).getContext("2d");
+                var genderChart = new Chart(genderChartContext).Pie(genderData, {});
+                $("#genderChartLegend").append(genderChart.generateLegend());
 
-                    ]
-                };
-                var ageChartElement = $("#ageChart");
-                var ageChartContext = ageChartElement.get(0).getContext("2d");
-                var ageChart = new Chart(ageChartContext).Bar(ageData, {});
-                $("#ageChartLegend").append(ageChart.generateLegend());
-
-                var religionData = [
-                    {
-                        value: 207176162,
-                        color: "Red",
-                        label: "Islam"
-                    },
-                    {
-                        value: 16528513,
-                        color: "Green",
-                        label: "Kristen"
-                    },
-                    {
-                        value: 6907873,
-                        color: "Blue",
-                        label: "Katolik"
-                    },
-                    {
-                        value: 4012116,
-                        color: "Cyan",
-                        label: "Hindu"
-                    },
-                    {
-                        value: 1703254,
-                        color: "Magenta",
-                        label: "Budha"
-                    },
-                    {
-                        value: 117091,
-                        color: "Yellow",
-                        label: "Kong Hu Chu"
-                    },
-                    {
-                        value: 299617,
-                        color: "Black",
-                        label: "Lainnya"
-                    }
-                ];
+                var religionData = <?= json_encode($chartjsAgama); ?>;
                 var religionChartElement = $("#religionChart");
                 var religionChartContext = religionChartElement.get(0).getContext("2d");
                 var religionChart = new Chart(religionChartContext).Pie(religionData, {});
