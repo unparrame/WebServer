cdedd39cde75335d116fe4dcbb4ba112ad46bcb6
vinsensiusvey <vinsensiusvey@gmail.com>
2015-10-24 13:11:49 +0700

    Pengurangan code
    Tidak terpakai lagi

--- www/application/controllers/Posters.php
+++ www/application/controllers/Posters.php
@@ -30,12 +30,6 @@ class Posters extends CI_Controller {
 
 	public function upload()
 	{
-		if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header("Access-Control-Allow-Origin: *");
-    header('Access-Control-Allow-Credentials: true');
-    header("Access-Control-Allow-Methods: GET, POST");
-		}
 		if(isset($_POST['posterEmail'])){
       $image = $_POST['image'];
       $this->base64toimg($image);
